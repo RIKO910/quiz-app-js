4f35a106b6ab130084d8ebf689eb7d8821f32bf8
RIKO910 <tarikulislamriko910@gmail.com>
2022-09-17 10:37:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const quizData = [
+    {
+        question:'How old is Florin?',
+        a:'10',
+        b:'17',
+        c:'26',
+        d:'18',
+
+        correct:'c'
+    },
+    {
+        question:'What is best programmin language in 2022',
+        a:'c',
+        b:'c++',
+        c:'python',
+        d:'java',
+        correct:'c'
+    },
+    {
+        question:'Who is president of Bangladesh',
+        a:'Riko',
+        b:'Tarikul',
+        c:'Abdul Hamid',
+        d:'Syed',
+        correct:'c'
+    },
+    {
+        question:'what does HTML for?',
+        a:'Hypertext Markup Language',
+        b:'Cascading Style Sheet',
+        c:'.......',
+        d:'......',
+        correct:'a'
+    }
+]
